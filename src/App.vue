ad91eab3eda2ef7711c6df29ca27559e3e7f0c8d
anhtuan <anhtuan6294@gmail.com>
2020-09-14 14:31:21 +0700

    Computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,52 @@
 <template>
-<HelloWorld msg="Vue 3" />
-<p>capacity: {{capacity}}</p>
-<button @click="increaseCapacity()">increaseCapacity</button>
+  <HelloWorld msg="Vue 3" />
+  <p>Spaces left {{spaceLeft}} from {{capacity}}</p>
+  <p>capacity: {{capacity}}</p>
+  <button @click="increaseCapacity()">increaseCapacity</button>
+  <h2>Attending</h2>
+  <ul>
+    <li v-for="(item,index) in attending" :key="index">{{item}}</li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import {
-    ref
-} from 'vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { computed, ref } from "vue";
 
 export default {
-    name: 'App',
-    components: {
-        HelloWorld
-    },
-    setup() {
-        const capacity = ref(3);
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const capacity = ref(3);
+    const attending = ref(["john", "kenny", "bob"]);
+    const spaceLeft = computed(() => {
+      return capacity.value - attending.value.length;
+    });
 
-        function increaseCapacity() {
-            console.log('increaseCapacity')
-            capacity.value++;
-        }
-
-        return {
-            capacity,
-            increaseCapacity
-        }
+    function increaseCapacity() {
+      console.log("increaseCapacity");
+      capacity.value++;
     }
-}
+
+    return {
+      capacity,
+      attending,
+      spaceLeft,
+      increaseCapacity,
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
