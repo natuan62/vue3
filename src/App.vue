5bd542b1ef645998c439ef184321cb8f25978916
anhtuan <anhtuan6294@gmail.com>
2020-09-14 14:20:56 +0700

    Setup ref

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<HelloWorld msg="Vue 3" />
+<div>capacity: {{capacity}}</div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {
+    ref
+} from 'vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    setup() {
+        const capacity = ref(3);
+
+        return {
+            capacity
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="hello">
+<div class="hello">
     <h1>{{ msg }}</h1>
-   
-  </div>
+
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    components: {
+
+    },
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
+
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
