c03bcd27733bf2d248476f545e5e1de325bb79e7
anhtuan <anhtuan6294@gmail.com>
2020-09-14 17:32:10 +0700

    Multi v-model

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <h2>Hello {{message}}</h2>
   <BaseInput v-model="inputValue" />
+  <h2>Salutations component</h2>
+  <SalutationName v-model:salutation="salutation" v-model:name="name" />
+  <button @click="viewValue()">View value</button>
 </template>
 <script>
-import { ref } from "vue";
+import { reactive, toRefs } from "vue";
 import BaseInput from "@/components/BaseInput";
+import SalutationName from "@/components/SalutationName";
 export default {
   name: "App",
-  components: { BaseInput },
+  components: { BaseInput, SalutationName },
   setup() {
-    const message = ref("Vue 3");
+    // const message = ref("Vue 3");
 
     // new v-model
-    const inputValue = ref("");
-    return { message, inputValue };
+    // const inputValue = ref("");
+
+    // multi v-model
+    // const salutation = ref("");
+    // const name = ref("");
+
+    // return { salutation, name, message, inputValue };
+
+    const event = reactive({
+      message: "vue 3",
+      inputValue: "",
+      salutation: "",
+      name: "",
+    });
+
+    function viewValue() {
+      console.log("viewValue()", event.salutation, event.name);
+    }
+    // return { ...toRefs(event), viewValue };
+    return { ...toRefs(event), viewValue };
   },
 };
 </script>
---
+++ src/components/SalutationName.vue
@@ -0,0 +1,41 @@
+<template>
+  <select
+    name="salutation"
+    @change="$emit(
+      'update:salutation',
+      $event.target.value
+  )"
+  >
+    <option value>Choose...</option>
+    <option
+      :value="item"
+      v-for="(item, index) in salutations"
+      :key="index"
+      :selected="salutation === item"
+    >{{item}}</option>
+  </select>
+
+  <input type="text" :value="name" @input="$emit(
+    'update:name',
+    $event.target.value
+  )" />
+</template>
+<script>
+const salutations = ["Mrs.", "Mr.", "Dr."];
+export default {
+  name: "SalutaionName",
+  props: {
+    salutation: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+  },
+  setup() {
+    return { salutations };
+  },
+};
+</script>
